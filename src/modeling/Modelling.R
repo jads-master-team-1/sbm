03494929319dfde96842d15ac305ad26346aca8e
laurenss8 <github_uni.fishers@aleeas.com>
2021-11-26 13:05:04 +0100

    several modeling fixes

--- src/modeling/Modelling.R
+++ src/modeling/Modelling.R
@@ -22,6 +22,24 @@ fintech <- fintech[c(numeric_columns, factor_columns, list_columns)]
 ### Set is last.funding.type Undisclosed to NA
 fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Undisclosed"] <- NA
 
+### making a further selection of last.funding.type
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Pre-Seed"] <- "Pre-Seed & Angel"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Angel"] <- "Pre-Seed & Angel"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Equity Crowdfunding"] <- "Crowdfunding & ICO"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Initial Coin Offering"] <- "Crowdfunding & ICO"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Product Crowdfunding"] <- "Crowdfunding & ICO"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Convertible Note"] <- NA
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Corporate Round"] <- NA
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Debt Financing"] <- NA
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Grant"] <- NA
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Non-equity Assistance"] <- NA
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Post-IPO Debt"] <- "Post-IPO"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Post-IPO Equity"] <- "Post-IPO"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Secondary Market"] <- "Post-IPO"
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Secondary Market"] <- NA
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Non-equity Assistance"] <- NA
+fintech$Last.Funding.Type[fintech$Last.Funding.Type == "Venture - Series Unknown"] <- NA
+
 ### Set is number.of.employees 10-Jan to 1-10
 fintech$Number.of.Employees[fintech$Number.of.Employees == "10-Jan"] <- "1-10"
 
@@ -34,7 +52,14 @@ fintech$Estimated.Revenue.Range[fintech$Estimated.Revenue.Range == "$10B+"] <- "
 fintech$Estimated.Revenue.Range[fintech$Estimated.Revenue.Range == "$1B to $10B"] <- "$100M+"
 fintech$Estimated.Revenue.Range[fintech$Estimated.Revenue.Range == "$500M to $1B"] <- "$100M+"
 fintech$Estimated.Revenue.Range[fintech$Estimated.Revenue.Range == "$100M to $500M"] <- "$100M+"
-# convert to "$500m+"
+
+# Create additional numeric variant of Estimated.Revenue.Range, namely Revenue
+fintech$Revenue <- matrix(0, nrow = nrow(fintech), ncol = 1)
+fintech$Revenue[fintech$Estimated.Revenue.Range == "Less than $1M"] <- 0
+fintech$Revenue[fintech$Estimated.Revenue.Range == "$1M to $10M"] <- 1
+fintech$Revenue[fintech$Estimated.Revenue.Range == "$10M to $50M"] <- 2
+fintech$Revenue[fintech$Estimated.Revenue.Range == "$50M to $100M"] <- 3
+fintech$Revenue[fintech$Estimated.Revenue.Range == "$100M+"] <- 4
 
 # Employee range: group the top three ranges
 # table(fintech$Number.of.Employees)
@@ -91,18 +116,21 @@ fintech$Total.Funding.Amount.Currency..in.USD. <- log(fintech$Total.Funding.Amou
 ## Set Types
 
 ### Set numeric types
-fintech$CB.Rank..Company. <- gsub(",", "", fintech$CB.Rank..Company.)
+#fintech$CB.Rank..Company. <- gsub(",", "", fintech$CB.Rank..Company.) # not relevant anymore since we don't have CBrank
 fintech[numeric_columns] <- sapply(fintech[numeric_columns], as.numeric)
 
-### Set factor types
+### Set factor types and ordering them
 fintech$Company.Type <- relevel(as.factor(fintech$Company.Type),
                                 ref = "For Profit")
-fintech$Estimated.Revenue.Range <- relevel(as.factor(fintech$Estimated.Revenue.Range),
-                                           ref = "Less than $1M")
+# fintech$Estimated.Revenue.Range <- relevel(as.factor(fintech$Estimated.Revenue.Range),
+#                                            ref = "Less than $1M")
+fintech$Estimated.Revenue.Range <- factor(fintech$Estimated.Revenue.Range, levels = c("Less than $1M", "$1M to $10M", "$10M to $50M", "$50M to $100M", "$100M+"), ordered = TRUE) 
 fintech$Last.Funding.Type <- relevel(as.factor(fintech$Last.Funding.Type),
-                                     ref = "Pre-Seed")
-fintech$Number.of.Employees <- relevel(as.factor(fintech$Number.of.Employees),
-                                       ref = "1-10")
+                                     ref = "Pre-Seed & Angel")
+# fintech$Number.of.Employees <- relevel(as.factor(fintech$Number.of.Employees),
+#                                        ref = "1-10")
+fintech$Number.of.Employees <- factor(fintech$Number.of.Employees, levels = c("1-10", "11-50", "51-100", "101-250", "251-500", "501-1000", "1000+"), ordered = TRUE)
+
 
 ### Set list types
 fintech$Industry.Groups <- as.character(fintech$Industry.Groups)
@@ -246,18 +274,34 @@ for (row in 1:nrow(fintech))
 #   Software + Art + Hardware + State 
 # + Last.Funding.Type + Number.of.Investors + Total.Funding.Amount.Currency..in.USD.
 
-# testing some correlations
-cor(fintech$Total.Funding.Amount.Currency..in.USD., fintech$Number.of.Investors)
-cor(fintech$Age, fintech$Total.Funding.Amount.Currency..in.USD.)
-cor(fintech$Number.of.Investors, fintech$Age)
+# # testing some correlations
+# cor(fintech$Total.Funding.Amount.Currency..in.USD., fintech$Number.of.Investors)
+# cor(fintech$Age, fintech$Total.Funding.Amount.Currency..in.USD.)
+# cor(fintech$Number.of.Investors, fintech$Age)
+# 
+# cor.test(fintech$Total.Funding.Amount.Currency..in.USD.,fintech$State)
+# # Here the p value is small, enough to reject the hypothesis of independence
+# 
+# chisq.test(fintech$Estimated.Revenue.Range, fintech$Number.of.Employees)
+# chisq.test(fintech$Number.of.Employees, fintech$Last.Funding.Type)
+# chisq.test(fintech$Estimated.Revenue.Range, fintech$Last.Funding.Type)
+# # These chisq are all correlated
 
-cor.test(fintech$Total.Funding.Amount.Currency..in.USD.,fintech$State)
-# Here the p value is small, enough to reject the hypothesis of independence
+# # calculating correlations
+# newdata <- fintech
+# 
+# newdata <- fintech$Finance + fintech$Software + fintech$Art + fintech$Hardware + fintech$State
+# 
+# numericonly<-newdata[ , purrr::map_lgl(newdata, is.numeric)] # select numeric values
+# bootcamp2021::corrNetwork(
+#   newdata[ , purrr::map_lgl(newdata, is.numeric)], 
+#   remove_below_abs = .7)
 
-chisq.test(fintech$Estimated.Revenue.Range, fintech$Number.of.Employees)
-chisq.test(fintech$Number.of.Employees, fintech$Last.Funding.Type)
-chisq.test(fintech$Estimated.Revenue.Range, fintech$Last.Funding.Type)
-# These chisq are all correlated
+# Checking # of observations for each industry group
+sum(fintech$Software)
+sum(fintech$Art)
+sum(fintech$Hardware)
+sum(fintech$State)
 
 # Modelling results
 
@@ -269,80 +313,84 @@ library(brant)     # brant test for proportional odds assumption OLR
 # In general, a VIF above 10 indicates high correlation and is cause for concern. 
 # Some authors suggest a more conservative level of 2.5 or above
 library(regclass) 
-regclass::VIF(m2)
+#regclass::VIF(m2)
+
+# # create intercept
+# fintech$intercept <- matrix(data = 1, nrow = nrow(fintech), ncol = 1)
+
 
 
 ### Create control variable model
-m <- MASS::polr(Estimated.Revenue.Range ~ Age + Number.of.Employees + Number.of.Funding.Rounds  + 
+m <- MASS::polr(Estimated.Revenue.Range ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds  + 
                   Software + Art + Hardware + State 
                 + Last.Funding.Type , data = fintech, Hess=TRUE)
-
 #### Check summary
 summary(m)
-
 #### Check assumption proportionality in the proportional odds model for OLR
-brant::brant(m)
+brant::brant(m, by.var = TRUE)
 #### Multicolinearity
-regclass::VIF(m)
+m_VIF <- lm(Revenue ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds  + 
+              Software + Art + Hardware + State 
+            + Last.Funding.Type , data = fintech)
+regclass::VIF(m_VIF)
+
 
 ### Create H1 model (number of investors)
-m1 <- MASS::polr(Estimated.Revenue.Range ~ Age + Number.of.Employees + Number.of.Funding.Rounds + 
+m1 <- MASS::polr(Estimated.Revenue.Range ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds + 
             Software + Art + Hardware + State +
-             Number.of.Investors, data = fintech, Hess=TRUE)
-# remove the following control variable: Last.Funding.Type +
-
+             + Last.Funding.Type + Number.of.Investors, data = fintech, Hess=TRUE)
 #### Check summary
 summary(m1)
-
 #### Check assumption proportionality in the proportional odds model for OLR
-brant::brant(m1)
+brant::brant(m1, by.var = TRUE)
 #### Multicolinearity
-regclass::VIF(m1)
+m1_VIF <- lm(Revenue ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds + 
+               Software + Art + Hardware + State +
+               + Last.Funding.Type + Number.of.Investors, data = fintech)
+regclass::VIF(m1_VIF)
 
 
 ### Create H2 model: total funding amount  (+ total equity funding amount?)
-m2 <- MASS::polr(Estimated.Revenue.Range ~ Age + Number.of.Employees + Number.of.Funding.Rounds + 
+m2 <- MASS::polr(Estimated.Revenue.Range ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds + 
             Software + Art + Hardware + State + 
-             Total.Funding.Amount.Currency..in.USD., data = fintech, Hess=TRUE)
-# remove the following control variable: Last.Funding.Type +
-
+              Last.Funding.Type + Total.Funding.Amount.Currency..in.USD., data = fintech, Hess=TRUE)
 #### Check summary
 summary(m2)
-
 #### Check assumption proportionality in the proportional odds model for OLR
-brant::brant(m2)
+brant::brant(m2, by.var = TRUE)
 #### Multicolinearity
-regclass::VIF(m2)
+m2_VIF <- lm(Revenue ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds + 
+               Software + Art + Hardware + State + 
+               Last.Funding.Type + Total.Funding.Amount.Currency..in.USD., data = fintech)
+regclass::VIF(m2_VIF)
 
-
+#zeros <- matrix(0, nrow = 1, ncol = 24)
 ### Create H3 model: total funding amount  * last funding round type 
-m3 <- MASS::polr(Estimated.Revenue.Range ~ Age + Number.of.Employees + Number.of.Funding.Rounds + 
+#m3_initial <- c(m2$coefficients, zeros, m2$zeta)
+m3 <- MASS::polr(Estimated.Revenue.Range ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds + 
             Software + Art + Hardware + State + 
-            Last.Funding.Type * Total.Funding.Amount.Currency..in.USD., data = fintech, Hess=TRUE)
-
+            Last.Funding.Type + Total.Funding.Amount.Currency..in.USD.
+            + Last.Funding.Type:Total.Funding.Amount.Currency..in.USD., data = fintech, Hess=TRUE)
 #### Check summary
 summary(m3)
-
 #### Check assumption proportionality in the proportional odds model for OLR
-brant::brant(m3)
+brant::brant(m3, by.var = TRUE)
 #### Multicolinearity
-regclass::VIF(m3)
+m3_VIF <- lm(Revenue ~ 1 + Age + Number.of.Employees + Number.of.Funding.Rounds + 
+               Software + Art + Hardware + State + 
+               Last.Funding.Type * Total.Funding.Amount.Currency..in.USD., data = fintech)
+regclass::VIF(m3_VIF)
 
 
+# fintech$test <- fintech$Last.Funding.Type * fintech$Total.Funding.Amount.Currency..in.USD.
+
+# To make tables
+library(texreg) # to make tables: texreg::screenreg(m1)
+library(stargazer) # to make tables with more models
+
+texreg::screenreg(l = list(m, m1, m2))
 
 
-
-
-
-# calculating correlations
-newdata <- fintech
-
-newdata <- fintech$Finance + fintech$Software + fintech$Art + fintech$Hardware + fintech$State
-
-numericonly<-newdata[ , purrr::map_lgl(newdata, is.numeric)] # select numeric values
-bootcamp2021::corrNetwork(
-  newdata[ , purrr::map_lgl(newdata, is.numeric)], 
-  remove_below_abs = .7)
 
 
 
